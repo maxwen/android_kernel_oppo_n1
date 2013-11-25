16bba423ccec06e825d025bcd700fa54ed62b537
maxwen <max.weninger@gmail.com>
2013-11-25 23:25:57 +0100

    board-8064-display: new test values for bandwith

--- arch/arm/mach-msm/board-8064-display.c
+++ arch/arm/mach-msm/board-8064-display.c
@@ -161,8 +161,8 @@ static struct msm_bus_vectors mdp_ui_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 577474560 * 2,
-		.ib = 866211840 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -171,8 +171,8 @@ static struct msm_bus_vectors mdp_vga_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 605122560 * 2,
-		.ib = 756403200 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -181,8 +181,8 @@ static struct msm_bus_vectors mdp_720p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 660418560 * 2,
-		.ib = 825523200 * 2,
+		.ab = 230400000 * 2,
+		.ib = 288000000 * 2,
 	},
 };
 
@@ -191,8 +191,8 @@ static struct msm_bus_vectors mdp_1080p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 764098560 * 2,
-		.ib = 955123200 * 2,
+		.ab = 334080000 * 2,
+		.ib = 417600000 * 2,
 	},
 };
 
@@ -235,9 +235,9 @@ static struct msm_panel_common_pdata mdp_pdata =
 {
     .gpio = MDP_VSYNC_GPIO,
 	.mdp_max_clk = 266667000,
-	.mdp_max_bw = 3080000000UL,
-	.mdp_bw_ab_factor = 200,
-	.mdp_bw_ib_factor = 210,
+	.mdp_max_bw = 4290000000UL,
+	.mdp_bw_ab_factor = 115,
+	.mdp_bw_ib_factor = 200,
     .mdp_bus_scale_table = &mdp_bus_scale_pdata,
     .mdp_rev = MDP_REV_44,
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
